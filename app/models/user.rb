82152a7d2297dda10e0f5e8cc882b34f55cf2cf8
yakko-chn <o.ub5ryno16i@gmail.com>
2020-11-09 16:22:29 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 10 }
+  end
+  
+    VALID_EMAIL = /\A\S+@\S+\.\S+\z/.freeze
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL }
+  
+    VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates  :password, confirmation: true, format: { with: VALID_PASSWORD }
+    validates  :password_confirmation, presence: true
+
+end
