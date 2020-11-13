2df0a86748a45c826826581bad94de3a62290d7e
yakko-chn <o.ub5ryno16i@gmail.com>
2020-11-13 18:15:52 +0900

    単体テストの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
+    buy_day     { '2020/11/11' }
+    category_id { '5' }
+    food_id     { '10' }
+    size_id     { '15' }
+    sell_by     { '2020/11/14' }
+    memo        { '昨晩の残り' }
     
+    association :user
   end
 end
