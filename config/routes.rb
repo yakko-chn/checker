871663e33328f20b75eeb2e0c2ec213c3d457571
yakko-chn <o.ub5ryno16i@gmail.com>
2020-11-17 15:23:25 +0900

    検索機能の実装
    usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users do
+     get 'search' , on: :collection
+   end
+
   root to: 'items#main'
    get "/concept/page", to: "items#concept", as: "concept"
    get "/search/user/page", to: "items#search", as: "search"
