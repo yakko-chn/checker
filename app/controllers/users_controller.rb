305b90af0037becda481de880826925263453427
yakko-chn <o.ub5ryno16i@gmail.com>
2020-12-03 23:01:38 +0900

    ゲストログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     @user = User.search(params[:nickname])
     @item = Item.order('created_at ASC').where(user_id: @user.ids)
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.nickname = "Guest"
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+    end
+    sign_in user
+    redirect_to root_path
+  end
 end
